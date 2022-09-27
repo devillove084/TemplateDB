9c09ae9bbe1b1004aa9f6215c4c25c46bd899970
devillve084 <786537003@qq.com>
2022-09-27 17:13:47 +0800

    change base for my project

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,24 +2,24 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+    base: "/TemplateKV/",
 
-  dest: "./dist",
+    dest: "./dist",
 
-  locales: {
-    "/": {
-      lang: "en-US",
-      title: "Docs Demo",
-      description: "A docs demo for vuepress-theme-hope",
+    locales: {
+        "/": {
+            lang: "en-US",
+            title: "Docs Demo",
+            description: "A docs demo for vuepress-theme-hope",
+        },
+        "/zh/": {
+            lang: "zh-CN",
+            title: "文档演示",
+            description: "vuepress-theme-hope 的文档演示",
+        },
     },
-    "/zh/": {
-      lang: "zh-CN",
-      title: "文档演示",
-      description: "vuepress-theme-hope 的文档演示",
-    },
-  },
 
-  theme,
+    theme,
 
-  shouldPrefetch: false,
+    shouldPrefetch: false,
 });
