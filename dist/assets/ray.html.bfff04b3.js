04fcda3b32a662206cc828bfbceac467d253820c
devillve084 <786537003@qq.com>
2022-09-27 17:37:02 +0800

    change base for my project in ci.yml

---
+++ dist/assets/ray.html.bfff04b3.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as t,c as o,a as r}from"./app.71256c4a.js";const a={},c=r("p",null,"Feature details here.",-1),s=[c];function _(n,l){return t(),o("div",null,s)}const f=e(a,[["render",_],["__file","ray.html.vue"]]);export{f as default};
