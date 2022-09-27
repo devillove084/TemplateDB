c40ac3b24d09e1b640b0bc00187ac46df5286d92
devillve084 <786537003@qq.com>
2022-09-27 17:45:35 +0800

    shabi

--- dist/assets/baz.html.72281050.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as t,c as o,a}from"./app.71256c4a.js";const c={},r=a("p",null,"Feature details here.",-1),s=[r];function _(n,l){return t(),o("div",null,s)}const f=e(c,[["render",_],["__file","baz.html.vue"]]);export{f as default};
